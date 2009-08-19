c31f7bd3741d2b9d5e91a758940d0791dd3ad776
Aleksey Palazhchenko <alek.silverstone@gmail.com>
2009-08-20 00:17:25 +0400

    Iterative chop.

---
+++ Kata2/Ruby/chop_iterative.rb
@@ -0,0 +1,19 @@
+def chop(value, array)
+  left_index = 0
+  right_index = array.size - 1
+  
+  while left_index <= right_index
+    middle_index = (left_index + (right_index - left_index) / 2).to_i
+    middle_value = array[middle_index]
+
+    if middle_value == value
+      return middle_index
+    elsif middle_value < value
+      left_index  = middle_index + 1
+    else
+      right_index = middle_index - 1
+    end
+  end
+    
+  return -1
+end
